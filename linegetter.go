91821d25f0cfb2578a6f154642f9320982d702bb
Marcin Drzymala <marcin.drzymala.mail@gmail.com>
2017-12-21 16:27:00 +0100

    Implement GetLine

--- linegetter.go
+++ linegetter.go
@@ -1,49 +1,86 @@
-// linegetter contains LineGetter interface which allows efficient reading
-// of random lines from huge files containing lot of lines, typically log files.
+// linegetter contains methods for efficient, random access reading
+// from huge files containing lot of lines, typically log files.
 package linegetter
 
 import (
-	"fmt"
 	"io"
 	"errors"
 )
 
-// LineGetter is an interface for retrieving concrete line from a file.
-// Lines can be accessed in random order. The delimiter for line separation
-// is the newline symbol. Newline symbol is platform specific.
-type LineGetter interface {
-	GetLineCount() uint64
-	GetLine(line uint64) (string, error)
+const (
+	read_chunk_sz := 16383 // 0x3FFF
+	MaxLineLength := read_chunk_sz
+)
+
+var (
+	ErrInvalidArgument = errors.New("invalid argument")
+	ErrLineTruncated   = errors.New("line truncated")
+)
+
+// LineGetter implements random access to lines from io.ReadSeeker object.
+// Lines are separated with ASCII line feed character, 0x0A in hex.
+type LineGetter struct {
+	total_line_count uint64
+	reader_seeker    io.ReadSeeker
 }
 
-// NewLineGetter creates a LineGetter from an io.ReadSeeker.
-// Whole ReadSeeker is scanned and indexed for GetLine speed.
-func NewLineGetter(rs io.ReadSeeker) (*lineGetter, error) {
+// NewLineGetter returns a new LineGetter.
+// Upon creation of LineGetter, the whole ReadSeeker is scanned.
+// If the io.ReadSeeker is nil, ErrInvalidArgument error is returned.
+func NewLineGetter(rs *io.ReadSeeker) *LineGetter {
 	if rs == nil {
-		return nil, errors.New("Nil value as argument to NewLineGetter()")
+		return nil, ErrInvalidArgument
 	}
-	lg := lineGetter{}
-	/* TODO: Implement me! */
+	lg := LineGetter{ total_line_count: 0, reader_seeker: rs }
+	lg.reindex()
 	return &lg, nil
 }
 
 // GetLineCount returns the number of lines available in the LineGetter.
-// Lines are split by newline sybmols. New line sybol is platform dependent.
-func (lg *lineGetter) GetLineCount() uint64 {
-	return lg.lineCount
+func (lg *LineGetter) GetLineCount() uint64 {
+	return lg.total_line_count
 }
 
-// GetLine returns the given line without the trailing newline symbols.
-// If the line number is not in range then error is returned.
-func (lg *lineGetter) GetLine(line uint64) (string, error) {
-	if line >= lg.GetLineCount() {
-		return "", errors.New(fmt.Sprintf("Line number out of range: %d/%d.", line, lg.GetLineCount()))
+// GetLine returns the n-th line from the LineGetter.
+// * If the line number is out of range, ErrInvalidArgument is returned.
+// * If some error happens during reading, the error is returned and
+//   the resulting string does not contain the full expected length.
+// * If the line length exceeds MaxLineLength, ErrLineTruncated is returned
+//   and the resulting string is truncated to MaxLineLength size.
+func (lg *LineGetter) GetLine(ln uint64) (string, error) {
+	if ln >= lg.total_line_count {
+		return "", ErrInvalidArgument
 	}
-	return "", nil
+	final_len := 0
+	truncated := false
+	start_idx := lg.li[ln]
+	end_index := lg.li[ln+1]
+	if end_index - start_idx > MaxLineLength {
+		truncated = true
+		final_len = MaxLineLength
+		end_index = start_idx + final_len
+	} else {
+		final_len = end_index - start_idx
+	}
+	buffer = make([]byte, final_len)
+	_, err := lg.reader_seeker.Seek(start_idx, io.SeekStart)
+	if err != nil {
+		return "", err
+	}
+	n, err = io.ReadFull(lg.reader_seeker, buffer)
+	if err != nil {
+		return buffer[:n], io.ErrUnexpectedEOF
+	}
+	if truncated {
+		return buffer, ErrLineTruncated
+	}
+	return buffer, nil
 }
 
-// lineGetter is the internal structure holding LineGetter data.
-type lineGetter struct {
-	lineCount uint64
-	rs io.ReadSeeker
+
+func (lg *LineGetter) reindex() {
+	lg.lineCount = 0
+	for {
+
+	}
 }
